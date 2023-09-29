4ac51d04bf1ef0138737513cab4f63c0e34ae09f
Bell Florian <Florian.BELL@htlvb.at>
2023-09-29 09:16:46 +0200

    Extract method to read a number

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,30 +1,29 @@
 ﻿Console.WriteLine("== Tiny Calculator ==");
 
-int divident;
-bool dividentBool;
-do
-{
-    Console.ResetColor();
-    Console.Write("Dividend: ");
-    dividentBool = int.TryParse(Console.ReadLine(), out divident);
-    Console.ForegroundColor = ConsoleColor.Red;
-    if (!dividentBool)
-        Console.WriteLine("Divident has to be a number!");
-} while (!dividentBool);
-
-int divisor;
-bool divisorBool;
-do
-{
-    Console.ResetColor();
-    Console.Write("Divisor: ");
-    divisorBool = int.TryParse(Console.ReadLine(), out divisor);
-    Console.ForegroundColor = ConsoleColor.Red;
-    if(!divisorBool)
-        Console.WriteLine("Divisor has to be a number!");
-    else if(divisor == 0)
-        Console.WriteLine("Divisor is not allowed to be 0!");
-} while (!divisorBool || divisor == 0);
+int divident = GetInt("Divident", false);
+int divisor = GetInt("Divisor", true);
 
 Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"{divident}/{divisor} = {divident/divisor}");
+
+static int GetInt(string name, bool isDivisor)
+{
+    int number;
+    bool isInt;
+    do
+    {
+        Console.ResetColor();
+        Console.Write($"{name}: ");
+        isInt = int.TryParse(Console.ReadLine(), out number);
+        Console.ForegroundColor = ConsoleColor.Red;
+        if (!isInt)
+            Console.WriteLine($"{name} has to be a number!");
+        else if( isDivisor && number == 0)
+        {
+            Console.WriteLine("Divisor is not allowed to be 0!");
+            isInt = false;
+        }
+    } while (!isInt);
+
+    return number;
+}
