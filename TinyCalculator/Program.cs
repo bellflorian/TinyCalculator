5f3948430fde871f8062e1b0e09f98851de39128
Bell Florian <Florian.BELL@htlvb.at>
2023-09-29 08:26:05 +0200

    Ask user for numbers until valid ones are entered

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -4,17 +4,27 @@ int divident;
 bool dividentBool;
 do
 {
+    Console.ResetColor();
     Console.Write("Dividend: ");
     dividentBool = int.TryParse(Console.ReadLine(), out divident);
+    Console.ForegroundColor = ConsoleColor.Red;
+    if (!dividentBool)
+        Console.WriteLine("Divident has to be a number!");
 } while (!dividentBool);
 
 int divisor;
 bool divisorBool;
 do
 {
+    Console.ResetColor();
     Console.Write("Divisor: ");
     divisorBool = int.TryParse(Console.ReadLine(), out divisor);
-} while (!divisorBool && divisor == 4);
+    Console.ForegroundColor = ConsoleColor.Red;
+    if(!divisorBool)
+        Console.WriteLine("Divisor has to be a number!");
+    else if(divisor == 0)
+        Console.WriteLine("Divisor is not allowed to be 0!");
+} while (!divisorBool || divisor == 0);
 
-Console.ForegroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"{divident}/{divisor} = {divident/divisor}");
